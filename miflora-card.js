71f1abf290a8634ec3afbdce2f939d7f652e491e
sli-cka <57716204+sli-cka@users.noreply.github.com>
2021-01-09 17:30:30 +0100

    Update miflora-card.js

--- miflora-card.js
+++ miflora-card.js
@@ -153,7 +153,7 @@ class MifloraCard extends HTMLElement {
                 margin-left: 15px;
                 margin-right: 15px;
                 margin-bottom: 15px;
-                margin-top: -5px;
+                margin-top: -10px;
                 width: 125px;
                 height: 125px;
                 border-radius: 6px;
